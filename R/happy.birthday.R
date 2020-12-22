065df580c3456ec0a8cebbf8fcbba9e96bf1766c
chris-dworschak <dworschak@posteo.de>
2020-12-22 09:17:36 +0100

    Adding manual WAV player file path argument

--- R/happy.birthday.R
+++ R/happy.birthday.R
@@ -5,6 +5,10 @@
 #' Can be set to "normal" for the usual tune,
 #' "exciting" for the use of a triplet,
 #' and "very exciting" for the use of a triplet and cadenza.
+#' @param set.player character string. If no default audio player for WAV files can be identified, 
+#' the function will return an error. In that case, a file path to an appropriate player needs to be manually set. 
+#' Use this argument to set a file path to an audio player on your system. In Windows, this might e.g. be
+#' "C:/Program Files/Windows Media Player/wmplayer.exe".
 #' @details The function _`happy.birthday`_ plays "Happy Birthday" in R.
 #' @import sound
 #' @import htmltab
@@ -17,12 +21,12 @@
 #' @export
 
 
-happy.birthday <- function(tune = "normal"){
+happy.birthday <- function(tune = "normal",
+                           set.player = NULL){
 
 if ( (is.null(tune) | !is.character(tune) | tune=="") == TRUE ) {
     stop('The argument "tune" must be set to "normal", "exciting", or "very exciting". ', call. = FALSE)
 }
-
 if(tune == "normal"){
 pitch <- c('D4', 'D4', 'E4', 'D4', 'G4', 'F#4',
            'D4', 'D4', 'E4', 'D4', 'A4', 'G4',
@@ -31,7 +35,6 @@ pitch <- c('D4', 'D4', 'E4', 'D4', 'G4', 'F#4',
 length <- c(rep(c(0.75, 0.25, 1, 1, 1, 2), 2),
            0.75, 0.25, 1, 1, 1, 1, 1, 0.75, 0.25, 1, 1, 1, 3)
 }
-
 if(tune == "exciting"){
 pitch <- c('D4', 'D4', 'D4', 'E4', 'D4', 'G4', 'F#4',
           'D4', 'D4', 'D4', 'E4', 'D4', 'A4', 'G4',
@@ -40,7 +43,6 @@ pitch <- c('D4', 'D4', 'D4', 'E4', 'D4', 'G4', 'F#4',
 length <- c(rep(c(0.33, 0.33, 0.33, 1, 1, 1, 2), 2),
           0.33, 0.33, 0.33, 1, 1, 1, 1, 1, 0.33, 0.33, 0.33, 1, 1, 1, 3)
 }
-
 if(tune == "very exciting"){
 pitch <- c('D4', 'D4', 'D4', 'E4', 'D4', 'G4', 'F4', 'G4', 'F#4',
           'D4', 'D4', 'D4', 'E4', 'D4', 'A4', 'G4', 'A4', 'G4',
@@ -49,6 +51,17 @@ pitch <- c('D4', 'D4', 'D4', 'E4', 'D4', 'G4', 'F4', 'G4', 'F#4',
 length <- c(rep(c(0.33, 0.33, 0.33, 1, 1, 0.33, 0.33, 0.33, 2), 2),
           0.33, 0.33, 0.33, 1, 1, 1, 1, 0.33, 0.33, 2.33, 0.33, 0.33, 0.33, 1, 1, 0.33, 0.33, 0.33, 3)
 }
+
+if( is.null(set.player)==FALSE & (!is.character(set.player) | set.player=="") == TRUE ) {
+  stop('The argument "set.player" must be supplied with a file path (character string) to an audio player able to play WAV files.', call. = FALSE)
+}  
+if(is.null(set.player)==FALSE & is.character(set.player)==TRUE){ 
+  sound::setWavPlayer(set.player)
+}
+if( is.null(sound::findWavPlayer() ) ){
+  stop('No WAV audio player was identified. Supply the argument "set.player" with a file path (character string) to an audio player able to play WAV files.', call. = FALSE)
+}
+
 
 bday.df <- data.frame(pitch, length)
 
--- R/merry.christmas.R
+++ R/merry.christmas.R
@@ -3,7 +3,11 @@
 #' @description Play "Merry Christmas" using R.
 #' @param tune character string. Allows for alternative arrangements.
 #' Can be set to "normal" for the usual tune, and
-#' "exciting" for some alternate notes and rythms.
+#' "exciting" for a more playful version.
+#' @param set.player character string. If no default audio player for WAV files can be identified, 
+#' the function will return an error. In that case, a file path to an appropriate player needs to be manually set. 
+#' Use this argument to set a file path to an audio player on your system. In Windows, this might e.g. be
+#' "C:/Program Files/Windows Media Player/wmplayer.exe".
 #' @details The function _`merry.christmas`_ plays "Merry Christmas" in R.
 #' @import sound
 #' @import htmltab
@@ -15,16 +19,15 @@
 #' @export
 
 
-merry.christmas <- function(tune = "normal"){
+merry.christmas <- function(tune = "normal",
+                            set.player = NULL){
 
 if ( (is.null(tune) | !is.character(tune) | tune=="") == TRUE ) {
     stop('The argument "tune" must be set to "normal", "exciting", or "very exciting". ', call. = FALSE)
 }
-  
 if (  tune=="very exciting" ) {
     stop('A very exciting tune is not yet implemented for "Merry Christmas"; the elves are still working on that. ', call. = FALSE)
 }
-
 if(tune == "normal"){
 pitch <- c("C4", "F4", "F4", "G4", "F4", "E4", "D4", "D4",
            "D4", "G4", "G4", "A4", "G4", "F4", "E4", "C4",
@@ -39,7 +42,6 @@ length <- c(rep(c(1, 1, 0.5, 0.5, 0.5, 0.5, 1, 1), 3),
             rep(c(1, 1, 1, 1, 2), 2),
             1, 1, 1, 1, 1, 1, 0.5, 0.5, 1, 1, 1, 2)
 }
-
 if(tune == "exciting"){
   pitch <- c("C4", "F4", "F4", "G4", "F4", "G4", "D4", "D4",
              "D4", "G4", "G4", "A4", "G4", "A4", rep(c("E4", "G4"), 2), "E4", 
@@ -55,6 +57,16 @@ if(tune == "exciting"){
               0.5, 0.5, 1, 1, 1, 0.25, 0.25, 1.5,
               rep(c(1, 1, rep(0.5, 4), 2), 2),
               1, 1, 1, 1, 1, 1, rep(0.25, 4), 1, 1, 1, 0.25, 0.25, 1.5)
+}
+
+if( is.null(set.player)==FALSE & (!is.character(set.player) | set.player=="") == TRUE ) {
+    stop('The argument "set.player" must be supplied with a file path (character string) to an audio player able to play WAV files.', call. = FALSE)
+}  
+if(is.null(set.player)==FALSE & is.character(set.player)==TRUE){ 
+  sound::setWavPlayer(set.player)
+}
+if( is.null(sound::findWavPlayer() ) ){
+  stop('No WAV audio player was identified. Supply the argument "set.player" with a file path (character string) to an audio player able to play WAV files.', call. = FALSE)
 }
 
 christmas.df <- data.frame(pitch, length)
